20f200a8042b3acc72e30e2e51e01d62542d798c
Peter Doohan <peterdoohan0@gmail.com>
2025-04-02 07:32:30 +0000

    all major lfp analyses done

--- GridMaze/analysis/event_aligned/lfp2.py
+++ GridMaze/analysis/event_aligned/lfp2.py
@@ -40,90 +40,101 @@ with open(EXPERIMENT_INFO_PATH / "maze_day2date.json", "r") as input_file:
 
 FS = 1500  # lfp sampling frequency
 
-# %% Main Functions
+# %% Spectrogram plots
 
 
-def plot_exp_average_spectrograms(signal_type, axes=None, smooth_SD=False):
-    if axes is None:
-        fig, axes = plt.subplots(1, 2, figsize=(10, 4), width_ratios=[0.9, 1])
-    if signal_type == "CSD":
-        save_path = LFP_RESULTS / "aligned_spectrograms_CSD.parquet"
-    elif signal_type == "LFP":
-        save_path = LFP_RESULTS / "aligned_spectrograms_LFP.parquet"
-    else:
-        NotImplementedError
-
-    df = load_data._load_multiindex_parquet(save_path)
-    # issue with freqs having diffent precisions HACK fix
-    df[("frequencies", "")] = df.frequencies.apply(lambda f: round(f, 5))
-    df = df[df.late_session]
-    cue_df = df.groupby("frequencies").cue_aligned_time.mean()
-    reward_df = df.groupby("frequencies").reward_aligned_time.mean()
-    # get common max, min values
-    _max = max(cue_df.max().max(), reward_df.max().max())
-    _min = min(cue_df.min().min(), reward_df.min().min())
-    freqs = cue_df.index.values
-    times = df.cue_aligned_time.columns.astype("float")
-    events = ["cue", "reward"]
-    for i, _df in enumerate([cue_df, reward_df]):
-        spec = _df.values
-        if smooth_SD:
-            spec = gaussian_filter(spec, sigma=smooth_SD)
-        cbar = True if i == 1 else False
-        _plot_spectrogram(spec, times, freqs, ax=axes[i], _min=_min, _max=_max, colorbar=cbar)
-        axes[i].set_title("")
-        axes[i].set_xlabel(f"{events[i]}-aligned time (s)")
-        if i == 1:
-            axes[1].set_ylabel("")
-    fig.tight_layout()
-
-
-def load_spectrogram_dfs_from_disk(signal_type="CSD", subject_IDs="all"):
-    save_paths = list((LFP_RESULTS / "aligned_spectrograms" / signal_type).iterdir())
-    if subject_IDs != "all":
-        save_paths = [p for p in save_paths if p.name.split(".")[0] in subject_IDs]
-    dfs = []
-    for p in save_paths:
-        print(f"loading {p.name}")
-        dfs.append(load_data._load_multiindex_parquet(p))
-    return dfs
-
-
-# %% Plotting functions (TODO: refac)
-
-
-def _plot_spectrogram_from_df(df, axes=None, window=False):
+def plot_cue_aligned_spectrogram_residuals(
+    spectrogram_df,
+    events=["cue_goal_directed", "cue_non_goal_directed"],
+    axes=None,
+    window=(-1, 1),
+    bands=[(4, 5), (7, 9), (9, 12)],
+):
     """ """
-    _df = df.copy()
+    # prepare axes
     if axes is None:
-        fig, axes = plt.subplots(1, 2, figsize=(10, 4), width_ratios=[0.9, 1])
-    if window:
-        new_columns = []
-        for col in _df.columns:
-            if col[0] in ["cue_aligned_time", "reward_aligned_time"]:
-                time_val = float(col[1])
-                if window[0] <= time_val <= window[1]:
-                    new_columns.append(col)
-            else:
-                new_columns.append(col)  # Keep metadata columns
-        _df = _df[new_columns]
-    # get common max, min values
-    _max = _df[["cue_aligned_time", "reward_aligned_time"]].max().max()
-    _min = _df[["cue_aligned_time", "reward_aligned_time"]].min().min()
-    freqs = _df.frequencies.values
-    for i, event in enumerate(["cue", "reward"]):
-        times = _df[f"{event}_aligned_time"].columns.astype("float")
-        spec = _df[f"{event}_aligned_time"].values
-        cbar = True if i == 1 else False
-        _plot_spectrogram(spec, times, freqs, ax=axes[i], _min=_min, _max=_max, colorbar=cbar)
-        axes[i].set_title("")
-        axes[i].set_xlabel(f"{event}-aligned time (s)")
-        if i == 1:
-            axes[1].set_ylabel("")
+        f, axes = plt.subplots(1, 2, figsize=(6, 3), width_ratios=[1, 0.75])
+    axes[1].spines[["top", "right"]].set_visible(False)
+    axes[1].set_xlabel("Time (s)")
+
+    # process data for spectrogram plot
+    df = spectrogram_df[spectrogram_df.late_session]
+    event_dfs = [df[df.event == e] for e in events]
+    av_spec_dfs = [_df.groupby("frequency").time.mean().time for _df in event_dfs]
+    residual_df = av_spec_dfs[0] - av_spec_dfs[1]
+    t = residual_df.columns.values.astype(np.float64)
+    freqs = residual_df.index.values
+    spec = residual_df.values
+    t_mask = (t >= window[0]) & (t <= window[1])
+    _min = spec[:, t_mask].min()
+    _max = spec[:, t_mask].max()
+    # plot
+    _plot_spectrogram(spec, t, freqs, ax=axes[0], _min=_min, _max=_max)
+    axes[0].set_xlim(*window)
+
+    # process data for band power plot
+    band_results = np.zeros((len(SUBJECT_IDS), len(bands), df.time.shape[1]))
+    for i, subject in enumerate(SUBJECT_IDS):
+        subject_df = df[df.subject_ID == subject]
+        event_dfs = [subject_df[subject_df.event == e] for e in events]
+        av_spec_dfs = [_df.groupby("frequency").time.mean().time for _df in event_dfs]
+        residual_df = av_spec_dfs[0] - av_spec_dfs[1]
+        for j, band in enumerate(bands):
+            band_df = residual_df.loc[residual_df.reset_index().frequency.between(*band).values]
+            band_mean = band_df.mean(axis=0)
+            band_results[i, j, :] = band_mean
+    band_results_mean = band_results.mean(axis=0)
+    band_results_sem = band_results.std(axis=0) / np.sqrt(len(SUBJECT_IDS))
+    # plot
+    for i, band in enumerate(bands):
+        band_mean = band_results_mean[i, :]
+        band_sem = band_results_sem[i, :]
+        axes[1].plot(t, band_mean, label=f"{band[0]}-{band[1]} Hz")
+        axes[1].fill_between(t, band_mean - band_sem, band_mean + band_sem, alpha=0.2)
+    axes[1].legend()
+    axes[1].set_xlim(*window)
+    axes[1].axhline(0, color="black", linestyle="--")
+    f.tight_layout()
+
+
+def plot_average_spectrogram(
+    spectrogram_df, axes=None, windows={"cue": (-1, 1), "reward": (-3, 3), "end_reward_consumption": (-1, 1)}
+):
+    """ """
+    # prepare axes
+    if axes is None:
+        fig, axes = plt.subplots(1, 3, figsize=(6, 3), width_ratios=[0.4, 1, 0.5], sharey=True)
+    # process data for plot
+    events = ["cue", "reward", "end_reward_consumption"]
+    df = spectrogram_df[spectrogram_df.late_session]
+    event_dfs = [df[df.event == e] for e in events]
+    av_spec_dfs = [_df.groupby("frequency").time.mean().time for _df in event_dfs]
+    _max = max([av_spec_df.max().max() for av_spec_df in av_spec_dfs])
+    _min = min([av_spec_df.min().min() for av_spec_df in av_spec_dfs])
+    for i, (ax, av_spec_df) in enumerate(zip(axes, av_spec_dfs)):
+        t = av_spec_df.columns.values.astype(np.float64)
+        spec = av_spec_df.values
+        freqs = av_spec_df.index.values
+        cbar = True if i == 2 else False
+        y_label = True if i == 0 else False
+        _plot_spectrogram(
+            spec,
+            t,
+            freqs,
+            ax=ax,
+            _min=_min,
+            _max=_max,
+            colorbar=cbar,
+            y_label=y_label,
+        )
+        event = events[i]
+        ax.set_title(event)
+        ax.set_xlim(*windows[event])
     fig.tight_layout()
+    return
 
 
-def _plot_spectrogram(x, times, freqs, ax=None, _min=None, _max=None, colorbar=True):
+def _plot_spectrogram(x, times, freqs, ax=None, _min=None, _max=None, colorbar=True, y_label=True):
     """ """
     if ax is None:
         fig, ax = plt.subplots(1, 1, figsize=(8, 4))
@@ -134,8 +145,8 @@ def _plot_spectrogram(x, times, freqs, ax=None, _min=None, _max=None, colorbar=T
     ax.axvline(0, color="white", linestyle="--")
     ax.grid(False)
     ax.set_yscale("log")
-    ax.set_title(f"Wavelet Decomposition")
-    ax.set_ylabel("Frequency (Hz)")
+    if y_label:
+        ax.set_ylabel("Frequency (Hz)")
     ax.set_xlabel("Time (s)")
     if colorbar:
         cbar = plt.colorbar(pcmesh, ax=ax, orientation="vertical")
@@ -144,7 +155,110 @@ def _plot_spectrogram(x, times, freqs, ax=None, _min=None, _max=None, colorbar=T
             spine.set_visible(False)
 
 
+# %% PSD plots
+
+
+def plot_PSD(
+    PSD_df,
+    axes=None,
+    normalise=False,
+):
+    """
+    Looks much better with LFP, maybe too low s/n in CSD
+    """
+    # prepare axes
+    if axes is None:
+        fig, axes = plt.subplots(1, 2, figsize=(6, 3), sharex=True, sharey=True)
+    for ax in axes:
+        ax.spines[["top", "right"]].set_visible(False)
+        ax.set_xlabel("Frequency (Hz)")
+        ax.set_ylabel("Power")
+        ax.set_xscale("log")
+    # process data for plot
+    df = PSD_df[PSD_df.late_session]
+    trial_events = ["cue", "reward", "ERC"]
+    trial_phases = ["navigation", "RC", "ITI"]
+    av_subject_psd = df.groupby(["subject_ID", "frequency"]).power.mean()
+    if normalise:
+        subject_max_power = av_subject_psd.groupby(level="subject_ID")["power"].max().max(1)
+        av_subject_psd = av_subject_psd.div(
+            av_subject_psd.index.get_level_values("subject_ID").map(subject_max_power), axis=0
+        )
+    subject_grouped_psd = av_subject_psd.reset_index("subject_ID").groupby("frequency").power
+    psd_mean = subject_grouped_psd.mean().power
+    psd_sem = subject_grouped_psd.sem().power
+    freqs = psd_mean.index.values
+    for ax, trial_sections in zip(axes, [trial_events, trial_phases]):
+        for s in trial_sections:
+            mean = psd_mean[s].values
+            sem = psd_sem[s].values
+            ax.plot(freqs, mean, label=s)
+            ax.fill_between(freqs, mean - sem, mean + sem, alpha=0.2)
+        ax.legend()
+
+
+# %% Event aligned signal plots
+
+
+def plot_av_subject_signal(
+    signal_df,
+    event="cue",
+    window=(-1, 1),
+    axes=None,
+):
+    """ """
+    # prepare axes
+    if axes is None:
+        fig, axes = plt.subplots(2, 3, figsize=(6, 4), sharey=True)
+    for ax in axes.flatten():
+        ax.spines[["top", "right"]].set_visible(False)
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("Amplitude")
+        ax.axhline(0, color="black", linestyle="--", alpha=0.2)
+        ax.axvline(0, color="black", linestyle="--", alpha=0.2)
+    # process data for plot
+    df = signal_df[signal_df.late_session]
+    df = df[df.event == event]
+    times = df.time.columns.values.astype(np.float64)
+    for subject, ax in zip(SUBJECT_IDS, axes.flatten()):
+        subject_df = df[df.subject_ID == subject]
+        mean = subject_df.time.mean(0).values
+        ax.plot(times, mean, c="k")
+        ax.set_title(subject)
+    for ax in axes.flatten():
+        ax.set_xlim(*window)
+    fig.tight_layout()
+
+
 # %% Load functions
+
+
+def load_spectrogram_df(signal_type):
+    """ """
+    save_path = LFP_RESULTS / f"aligned_spectrograms_{signal_type}.parquet"
+    if not save_path.exists():
+        print(f"event aligned spectrogram dfs not found, save with fn: save_all_spectrogram_dfs")
+    df = load_data._load_multiindex_parquet(save_path)
+    return df
+
+
+def load_PSD_df(signal_type):
+    """ """
+    save_path = LFP_RESULTS / f"trial_PSD_{signal_type}.parquet"
+    if not save_path.exists():
+        print(f"event aligned PSD dfs not found, save with fn: save_all_PSD_dfs")
+    df = load_data._load_multiindex_parquet(save_path)
+    return df
+
+
+def load_signal_df(signal_type):
+    """ """
+    save_path = LFP_RESULTS / f"aligned_{signal_type}.parquet"
+    if not save_path.exists():
+        print(f"event aligned signal dfs not found, save with fn: save_all_signal_dfs")
+    df = load_data._load_multiindex_parquet(save_path)
+    return df
+
 
 # %% Save functions
 
@@ -205,6 +319,7 @@ def save_all_spectrogram_dfs(overwrite=False, signal_type="CSD", verbose=False, 
                     if verbose:
                         print(e)
                         print(f"Failed to generate spectrogram for session: {session.name}")
+    save_path = LFP_RESULTS / f"aligned_spectrograms_{signal_type}.parquet"
     combined_dfs = pd.concat(spectrogram_dfs, axis=0).reset_index(drop=True)
     print(f"Saving {save_path.name}")
     combined_dfs.columns = combined_dfs.columns.map(lambda x: str(x))
