b643a4e880556678b9284bfd97ecc1aa807f8f52
Peter Doohan <peterdoohan0@gmail.com>
2025-04-27 20:18:52 +0000

    updated get_dist_info

--- GridMaze/analysis/distance_to_goal/gd2.py
+++ GridMaze/analysis/distance_to_goal/gd2.py
@@ -380,10 +380,12 @@ def get_distance_aligned_input_data(
     session,
     resolution=0.5,
     include_multi_units=True,
+    include_trial_phases=["navigation", "reward_consumption", "ITI"],
+    ignore_last_n=2,
     binning_method="uniform",
     n_bins=25,
     max_steps_to_goal=25,
-    include_place_onehots=True,
+    include_place_onehots=False,
 ):
     """
     Returns a dataframe with spike counts aligned to future path-distance to goal over all trials in a session.
@@ -403,7 +405,11 @@ def get_distance_aligned_input_data(
     spike_counts_df = spike_counts_df[spike_counts_df.columns[spike_counts_df.spike_count.columns.isin(keep_clusters)]]
     # downsample data
     ds_nav_info, ds_spike_counts_df = _downsample_data(navigation_df, spike_counts_df, resolution)
-    return ds_nav_info
+    # update distances in non_navigation trial periods
+    if "ITI" in include_trial_phases or "reward_consumption" in include_trial_phases:
+        ds_nav_info[("steps_to_goal", "future")] = _add_non_nav_distances(
+            session, ds_nav_info, ignore_last_n=ignore_last_n
+        )
     # add distance bins info
     bins = convert._get_distance_bins(
         binning_method=binning_method,
@@ -411,74 +417,24 @@ def get_distance_aligned_input_data(
         distance_metrics=("steps_to_goal", "future"),
         max_distance=max_steps_to_goal,
     )
-
     ds_nav_info[("steps_to_goal", "bin")] = pd.cut(ds_nav_info.steps_to_goal.future, bins=bins)
     ds_nav_info[("steps_to_goal", "bin_mid")] = ds_nav_info.steps_to_goal.bin.apply(lambda x: x.mid).astype(float)
     # add event aligned time info (for later cross-decoder comparisons)
     ds_nav_info[("event_aligned_time", "cue")] = _get_event_aligned_times(ds_nav_info, trials_df, "cue")
     ds_nav_info[("event_aligned_time", "reward")] = _get_event_aligned_times(ds_nav_info, trials_df, "reward")
-    # combine and filter for navigation only and to max_steps_to_goal
+    # combine and filter out points where distance is not defined
     ds_nav_rates_df = pd.concat([ds_nav_info, ds_spike_counts_df], axis=1)
-    ds_nav_rates_df = ds_nav_rates_df[ds_nav_rates_df.steps_to_goal.future.le(max_steps_to_goal)]
+    ds_nav_rates_df = ds_nav_rates_df[~ds_nav_rates_df.steps_to_goal.future.isna()]
     ds_nav_rates_df[("trial", "")] = ds_nav_rates_df[("trial", "")].astype(int)
     # include position inputs for decoder (optional)
     if include_place_onehots:
         place_onehots_df = _get_place_onehots_df(session, ds_nav_rates_df)
-        # combine with other data (nav_info and spike_counts)
         ds_nav_rates_df = pd.concat([ds_nav_rates_df, place_onehots_df], axis=1)
     return ds_nav_rates_df
 
 
 def _add_non_nav_distances(session, nav_info_df, ignore_last_n=2):
-    """
-    Fills in NaN distances to goal info for RC and ITI periods.
-    Distance in these instances is defined as the shortest path to the NEXT goal cued
-    Note ITI periods are defined at the end of the trial, so we need to look up the next trial's
-    goal to calc distances
-    """
-    # load additional data
-    trials_df = session.trials_df
-    last_trial = session.trials_df.trial.max()
-    trials_df = trials_df.set_index("trial")
-    simple_maze = session.simple_maze()
-
-    # precalculate distances to goal
-    extended_simple_maze = mr.get_extended_simple_maze(simple_maze)
-    path_distances = dict(nx.all_pairs_dijkstra_path_length(extended_simple_maze, weight="weight"))
-    label2coord = mr.get_maze_label2coord(simple_maze)
-
-    # get distances to goal for non-navigation periods
-    new_distances = nav_info_df.steps_to_goal.future.copy()
-    for trial in nav_info_df.trial.dropna().unique():
-        trial_df = nav_info_df[nav_info_df.trial == trial]
-        max_windows = trial_df.shape[0]
-        if ignore_last_n:
-            max_windows = max_windows - ignore_last_n
-        window_count = 0
-        for i, row in trial_df.iterrows():
-            print(i)
-            window_count += 1
-            current_dist = row.steps_to_goal.future
-            if not np.isnan(current_dist):
-                continue
-            else:
-                if window_count > max_windows:
-                    continue
-                trial_phase = row[("trial_phase", "")]
-                assert trial_phase in ["reward_consumption", "ITI"], f"Unexpected trial phase: {trial_phase}"
-                trial = row[("trial", "")]
-                if trial == last_trial:
-                    continue  # no next goal defined
-                next_trial = trials_df.loc[trial + 1]
-                next_goal = next_trial[("goal", "")]
-                pos = row.maze_position.simple
-                dist = path_distances[label2coord[pos]][label2coord[next_goal]]
-                new_distances.loc[i] = dist
-
-    return new_distances
-
-
-def _add_non_nav_distances2(session, nav_info_df, ignore_last_n=2):
+    """ """
     # load additional data
     trials_df = session.trials_df
     trials_df = trials_df.set_index(("trial", ""))
