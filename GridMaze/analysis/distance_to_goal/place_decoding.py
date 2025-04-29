17a84520055b3c2e6d3c82683f9af0e1e623a5a5
Peter Doohan <peterdoohan0@gmail.com>
2025-04-29 09:14:14 +0000

    new file for place_decoding analyses

---
+++ GridMaze/analysis/distance_to_goal/place_decoding.py
@@ -0,0 +1,11 @@
+"""
+Library for decoding subject location (place) as a function of distance to goal of event
+aligned time. Uses util functions in ./decoding_utils.py
+@peterdoohan
+"""
+
+# %% Imports
+
+# %% Global Variables
+
+# %% Functions
