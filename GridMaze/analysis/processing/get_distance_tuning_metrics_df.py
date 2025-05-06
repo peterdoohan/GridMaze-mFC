3c2d173a2504eaa99f6ab3fd3f54b52949491f97
Peter Doohan <peterdoohan0@gmail.com>
2025-05-06 13:40:51 +0000

    new file for isolated calc of distance tuning metrics to be used in various other analyses

---
+++ GridMaze/analysis/processing/get_distance_tuning_metrics_df.py
@@ -0,0 +1,12 @@
+"""
+Updated lib for calculating distance to goal tuning parameters (define basis fits etc.)
+and saving out results to dataframe (clusters.DistanceTuningMetrics.parquet)
+"""
+
+# %% Imports
+
+
+# %% Global Variables
+
+
+# %% Functions
