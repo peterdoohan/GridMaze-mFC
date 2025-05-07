885ae7874408dd9944254fda7f7543040ff5d3aa
Peter Doohan <peterdoohan0@gmail.com>
2025-05-07 12:58:30 +0000

    updated hpc script to run all input output variants in one job (now just one job per session)

--- jobs/place_decoding/submit.py
+++ jobs/place_decoding/submit.py
@@ -22,26 +22,18 @@ def submit_all_jobs():
     for subject in SUBJECT_IDS:
         for maze_name in MAZE_DAY2DATE.keys():
             for day_on_maze in [int(d) for d in MAZE_DAY2DATE[maze_name].keys()]:
-                for training_trial_phases in ["navigation", "all"]:
-                    for input_type in ["spikes", "spikes_by_distance"]:
-                        for output_type in ["place_direction", "place"]:
-                            script_path = get_SLURM_script(
-                                subject,
-                                maze_name,
-                                day_on_maze,
-                                input_type,
-                                output_type,
-                                training_trial_phases,
-                            )
-                            os.system(f"chmod +x {script_path}")
-                            os.system(f"sbatch {script_path}")
+                script_path = get_SLURM_script(
+                    subject,
+                    maze_name,
+                    day_on_maze,
+                )
+                os.system(f"chmod +x {script_path}")
+                os.system(f"sbatch {script_path}")
 
 
-def get_SLURM_script(subject, maze_name, day_on_maze, input_type, output_type, training_trial_phases):
+def get_SLURM_script(subject, maze_name, day_on_maze):
     """ """
-    exp_name = (
-        f"{input_type}_{output_type}_decoding_{subject}_{maze_name}_{day_on_maze}_training_{training_trial_phases}"
-    )
+    exp_name = f"spatial_decoding_{subject}_{maze_name}_{day_on_maze}"
     script = f"""#!/bin/bash
 #SBATCH --job-name={exp_name}
 #SBATCH --output=jobs/place_decoding/out/{exp_name}.out
@@ -50,13 +42,26 @@ def get_SLURM_script(subject, maze_name, day_on_maze, input_type, output_type, t
 #SBATCH --cpus-per-task=12
 #SBATCH -p gpu
 #SBATCH --mem=16GB
-#SBATCH --time=1:00:00
+#SBATCH --time=12:00:00
 
 module load miniconda
 conda deactivate
 conda deactivate
 conda activate goalNav_mEC
-python -c \"from GridMaze.analysis.distance_to_goal import place_decoding as dp; dp.run_session_place_decoding(('{subject}', '{maze_name}', {day_on_maze}), '{input_type}', '{output_type}', '{training_trial_phases}')\"
+
+python - <<'PYCODE'
+from GridMaze.analysis.distance_to_goal import place_decoding as dp
+
+params = ('{subject}', '{maze_name}', {day_on_maze})
+dp.run_session_place_decoding(params, 'spikes', 'place', 'all')
+dp.run_session_place_decoding(params, 'spikes', 'place', 'navigation')
+dp.run_session_place_decoding(params, 'spikes', 'place_direction', 'all')
+dp.run_session_place_decoding(params, 'spikes', 'place_direction', 'navigation')
+dp.run_session_place_decoding(params, 'spikes_by_distance', 'place', 'all')
+dp.run_session_place_decoding(params, 'spikes_by_distance', 'place', 'navigation')
+dp.run_session_place_decoding(params, 'spikes_by_distance', 'place_direction', 'all')
+dp.run_session_place_decoding(params, 'spikes_by_distance', 'place_direction', 'navigation')
+PYCODE
 """
     script_path = f"jobs/place_decoding/slurm/{exp_name}.sh"
     with open(script_path, "w") as f:
