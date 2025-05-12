7a8d715fb5f5a2755169bae4e3caca8f6be104e9
Peter Doohan <peterdoohan0@gmail.com>
2025-05-12 10:03:30 +0000

    new files for goal-distance CPD analysis (does goal x distance explain unique variance of place-direction and distance)

---
+++ GridMaze/analysis/distance_to_goal/goal_cpd.py
@@ -0,0 +1,11 @@
+"""
+New library for goal-coding encoding analyses. Test if goal-distance explains unique variance over place-direction and distance
+in the neural population.
+@peterdoohan
+"""
+
+# %% Imports
+
+# %% Global Variables
+
+# %% Functions.
---
+++ GridMaze/analysis/place_direction/bases.py
@@ -0,0 +1,14 @@
+"""
+Library for generating bases that efficiently code for place-direction, used for encoding analyses.
+Eg, for given session, use all other late sessions from that subjects to generate place-direction bases and then
+use those bases to efficently ecnode place-direction for the current session.
+@peterdoohan
+"""
+
+# %% Imports
+
+
+# %% Global Variables
+
+
+# %% Functions
