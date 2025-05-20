cfbd3183055d35688fa63a5066867776b33916f4
Peter Doohan <peterdoohan0@gmail.com>
2025-05-20 16:24:31 +0000

    CPD and beta distance_metric_comparisons working with Poission GLM

--- GridMaze/analysis/distance_to_goal/cpd.py
+++ GridMaze/analysis/distance_to_goal/cpd.py
@@ -9,12 +9,14 @@ import pandas as pd
 from joblib import Parallel, delayed
 from sklearn.linear_model import Ridge, PoissonRegressor
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_poisson_deviance
 
 from GridMaze.analysis.core import get_sessions as gs
 from GridMaze.analysis.core import downsample as ds
 from GridMaze.analysis.core import folds
 from GridMaze.analysis.core import encoding_utils as eu
 from GridMaze.analysis.core import convert
+
 
 from GridMaze.analysis.distance_to_goal import bases as db
 from GridMaze.analysis.distance_to_goal import distributions as dd
@@ -26,13 +28,13 @@ from GridMaze.paths import EXPERIMENT_INFO_PATH
 with open(EXPERIMENT_INFO_PATH / "subject_IDs.json", "r") as input_file:
     SUBJECT_IDS = json.load(input_file)
 
-# %% Functions
+# %% L1, L2 ratio comparison function
 
 
 def compare_distance_metric_regression_weights(
     session,
-    resolution=0.5,
-    fixed_alpha=10,
+    resolution=0.25,
+    fixed_alpha=False,
     model="PoissonRegressor",
     n_bases=10,
     basis_type="gamma",
@@ -77,12 +79,13 @@ def compare_distance_metric_regression_weights(
         cluster_alphas = get_test_train_opt_alpha(folds_df, input_data, model=model)
     else:
         cluster_alphas = pd.Series(index=cluster_unique_IDs, data=fixed_alpha)
-    # fit each cluster in a Possion GLM with distance metric featrues
+    # get data to fit
     X = np.hstack([input_data.metric_1.values, input_data.metric_2.values])
     # ensure X is scaled when inperpretting betas
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
     Y = input_data.spike_count.values
+    # fit each cluster in a Linear OLS / Possion GLM with distance metric featrues
     results = []
     for i, cluster in enumerate(cluster_unique_IDs):
         y = Y[:, i]
@@ -112,17 +115,143 @@ def compare_distance_metric_regression_weights(
                 }
             )
     results_df = pd.DataFrame(results)
+    # add session info before returning
     results_df["subject_ID"] = session.subject_ID
     results_df["maze_name"] = session.maze_name
     results_df["day_on_maze"] = session.day_on_maze
     return results_df
 
 
+# %% CPD function
+
+
+def get_distance_metric_CPDs(
+    session,
+    resolution=0.5,
+    model="PoissonRegressor",
+    n_bases=10,
+    basis_type="gamma",
+    metric_1="geodesic",
+    metric_2="euclidean",
+    max_steps_to_goal=25,
+    max_jobs=20,
+):
+    """ """
+    # get input data
+    input_data = get_input_data(
+        session,
+        metric_1=("distance_to_goal", metric_1),
+        metric_2=("distance_to_goal", metric_2),
+        resolution=resolution,
+        max_steps_to_goal=max_steps_to_goal,
+    )
+    cluster_unique_IDs = input_data.spike_count.columns.values
+    # get a set of basis function activations for each distance metric
+    basis_activation_dfs = []
+    for i, m in enumerate([metric_1, metric_2]):
+        _m = ("distance_to_goal", m)
+        basis_fn = db.distance_basis_generator(
+            n_bases=n_bases,
+            basis=basis_type,
+            btype="distance",
+            max_distance=dd.get_distance_percentile(_m, percentile=85),
+        )
+        basis_activations = basis_fn(input_data[_m])
+        basis_activations = pd.DataFrame(
+            basis_activations,
+            columns=pd.MultiIndex.from_product([[f"metric_{i+1}"], np.arange(0, n_bases)]),
+            index=input_data.index,
+        )
+        basis_activation_dfs.append(basis_activations)
+    # combine basis activations with input data
+    input_data = pd.concat([input_data, *basis_activation_dfs], axis=1)
+    folds_df = folds.get_folds_df(session, goal_stratified=False, n_folds=5)
+    _folds = folds_df.columns.get_level_values(0).unique()
+    model_name2regessor_classes = {
+        "full": ["metric_1", "metric_2"],
+        f"reduced_{metric_1}": ["metric_2"],
+        f"reduced_{metric_2}": ["metric_1"],
+    }
+    all_results = []
+    for fold in _folds:
+        fold_df = folds_df[fold]
+        fold_results = []
+        for model_name, regressor_classes in model_name2regessor_classes.items():
+            cluster_alphas = get_train_folds_opt_alpha(
+                fold_df, input_data, model=model, regressor_classes=regressor_classes
+            )
+            train_trials = fold_df["train"].unstack().dropna().values
+            test_trials = fold_df["test"].unstack().dropna().values
+            train_df = input_data[input_data.trial_unique_ID.isin(train_trials)]
+            test_df = input_data[input_data.trial_unique_ID.isin(test_trials)]
+            X_train, Y_train, X_test, Y_test = get_test_train_arrays(train_df, test_df, regressor_classes, scale_X=True)
+            model_results = Parallel(n_jobs=max_jobs)(
+                delayed(_process_cluster)(
+                    X_train,
+                    Y_train[:, i],
+                    X_test,
+                    Y_test[:, i],
+                    model,
+                    cluster_alphas.loc[cluster],
+                    cluster,
+                    fold,
+                    model_name,
+                )
+                for i, cluster in enumerate(cluster_unique_IDs)
+            )
+            fold_results.extend(model_results)
+        all_results.extend(fold_results)
+    df = pd.DataFrame(all_results)  # every cluster, model, model - socre
+    # calculate CPD values for metric_1 and metric_2 by comparing full and reudced models
+    metric = "deviance" if model == "PoissonRegressor" else "rss"
+    # average metric across folds
+    model_metrics = df.groupby(["cluster_unique_ID", "model_name"])[metric].mean().unstack()
+    cpd_df = pd.DataFrame(index=model_metrics.index)
+    for m in [metric_1, metric_2]:
+        reduced = model_metrics[f"reduced_{m}"]
+        full = model_metrics["full"]
+        cpd_df[m] = (reduced - full) / (reduced)
+    return cpd_df
+
+
+def _process_cluster(X_train, y_train, X_test, y_test, model, alpha, cluster, fold, model_name):
+    if model == "PoissonRegressor":
+        Model = PoissonRegressor(alpha=alpha, max_iter=10_000)
+        Model.fit(X_train, y_train)
+        y_pred = Model.predict(X_test)
+        score = Model.score(X_test, y_test)
+        deviance = mean_poisson_deviance(y_test, y_pred)
+        return {
+            "cluster_unique_ID": cluster,
+            "fold": fold,
+            "score": score,
+            "deviance": deviance,
+            "alpha": alpha,
+            "model_name": model_name,
+        }
+    elif model == "Ridge":
+        Model = Ridge(alpha=alpha, max_iter=10_000, random_state=0)
+        Model.fit(X_train, y_train)
+        y_pred = Model.predict(X_test)
+        score = Model.score(X_test, y_test)
+        rss = np.sum((y_test - y_pred) ** 2)
+        return {
+            "cluster_unique_ID": cluster,
+            "fold": fold,
+            "score": score,
+            "rss": rss,
+            "alpha": alpha,
+            "model_name": model_name,
+        }
+
+
 # %% Get Xvaled regularisation across either test_train splits or folds within training data
 
 
 def get_test_train_opt_alpha(folds_df, input_data, model="PoissonRegressor", max_jobs=20):
-    """ """
+    """
+    Returns best alpha (median across folds) for each cluster in input_data over test_train splits
+    """
     cluster_unique_IDs = input_data.spike_count.columns.values
     _folds = folds_df.columns.get_level_values(0).unique()
     results = []
@@ -133,7 +262,7 @@ def get_test_train_opt_alpha(folds_df, input_data, model="PoissonRegressor", max
         train_df = input_data[input_data.trial_unique_ID.isin(train_trials)]
         X_train, Y_train, X_test, Y_test = get_test_train_arrays(train_df, test_df, scale_X=True)
         fold_results = Parallel(n_jobs=max_jobs)(
-            delayed(_process_cluster)(fold, i, cluster, X_train, Y_train, X_test, Y_test, model=model)
+            delayed(_process_cluster_reg_search)(fold, i, cluster, X_train, Y_train, X_test, Y_test, model=model)
             for i, cluster in enumerate(cluster_unique_IDs)
         )
         results.extend(fold_results)
@@ -143,7 +272,32 @@ def get_test_train_opt_alpha(folds_df, input_data, model="PoissonRegressor", max
     return cluster_opt_alphas
 
 
-def _process_cluster(fold, i, cluster, X_train, Y_train, X_test, Y_test, model="PoissonRegressor"):
+def get_train_folds_opt_alpha(
+    fold_df, input_data, model="PoissonRegressor", regressor_classes=["metric_1", "metric_2"], max_jobs=20
+):
+    """ """
+    cluster_unique_IDs = input_data.spike_count.columns.values
+    train_df = fold_df["train"]
+    train_folds = train_df.columns.values
+    train_fold_results = []
+    for fold in train_folds:
+        vtest_trials = train_df[fold].dropna().values
+        vtrain_trials = train_df[[f for f in train_folds if f != fold]].unstack().dropna().values
+        vtrain_df = input_data[input_data.trial_unique_ID.isin(vtrain_trials)]
+        vtest_df = input_data[input_data.trial_unique_ID.isin(vtest_trials)]
+        X_train, Y_train, X_test, Y_test = get_test_train_arrays(vtrain_df, vtest_df, regressor_classes, scale_X=True)
+        fold_results = Parallel(n_jobs=max_jobs)(
+            delayed(_process_cluster_reg_search)(fold, i, cluster, X_train, Y_train, X_test, Y_test, model=model)
+            for i, cluster in enumerate(cluster_unique_IDs)
+        )
+        train_fold_results.extend(fold_results)
+    reg_df = pd.DataFrame(train_fold_results)
+    # get median best_alpha across folds
+    cluster_opt_alphas = reg_df.groupby(["cluster_unique_ID"]).best_alpha.median()
+    return cluster_opt_alphas
+
+
+def _process_cluster_reg_search(fold, i, cluster, X_train, Y_train, X_test, Y_test, model="PoissonRegressor"):
     print(cluster)
     y_train, y_test = Y_train[:, i], Y_test[:, i]
     best_alpha, best_score = eu.reg_search_regression(
@@ -157,26 +311,21 @@ def _process_cluster(fold, i, cluster, X_train, Y_train, X_test, Y_test, model="
     }
 
 
-def get_train_folds_opt_alpha():
-    """ """
-
-    return
-
-
 # %%
 
 
-def reg_search_PoissionRegression():
+def get_test_train_arrays(train_df, test_df, regressor_classes=["metric_1", "metric_2"], scale_X=True):
     """ """
-
-    return
-
-
-def get_test_train_arrays(train_df, test_df, scale_X=True):
-    """ """
-    X_train, X_test = np.hstack([train_df.metric_1.values, train_df.metric_2.values]), np.hstack(
-        [test_df.metric_1.values, test_df.metric_2.values]
-    )
+    X_train, X_test = [], []
+    if "metric_1" in regressor_classes:
+        X_train.append(train_df.metric_1.values)
+        X_test.append(test_df.metric_1.values)
+    if "metric_2" in regressor_classes:
+        X_train.append(train_df.metric_2.values)
+        X_test.append(test_df.metric_2.values)
+    if "metric_1" not in regressor_classes and "metric_2" not in regressor_classes:
+        raise ValueError("Must include at least one metric for input_features")
+    X_train, X_test = np.hstack(X_train), np.hstack(X_test)
     Y_train, Y_test = train_df.spike_count.values, test_df.spike_count.values
     # standardise
     if scale_X:
