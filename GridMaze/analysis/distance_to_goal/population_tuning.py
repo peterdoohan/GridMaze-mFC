9521fca711107c15f4552cdbf0cd55093b2f0bb5
Peter Doohan <peterdoohan0@gmail.com>
2025-05-23 14:08:36 +0000

    reimplementing code for distance tuning heatmaps

--- GridMaze/analysis/distance_to_goal/population_tuning.py
+++ GridMaze/analysis/distance_to_goal/population_tuning.py
@@ -3,12 +3,14 @@ Library for distance to goal tuning analyses: curve fits, headmaps etc.
 """
 
 # %% Imports
-from cProfile import label
 import json
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 from GridMaze.analysis.core import get_sessions as gs
+
+from GridMaze.analysis.cluster_tuning import distance_to_goal as dtg
+from GridMaze.analysis.distance_to_goal import distributions as dd
 
 # %% Global Variables
 from GridMaze.paths import EXPERIMENT_INFO_PATH
@@ -21,6 +23,73 @@ CURVE_FITS = ["gamma_4p", "gaussian_4p", "polynomial_4p"]
 
 
 # %%
+
+
+def get_population_tuning_df(late_sessions=True, min_split_half_corr=0.5, min_r2=0.5, fit="gamma_4p", verbose=False):
+    """ """
+    days_on_maze = "late" if late_sessions else "all"
+    if verbose:
+        print("Loading sessions...")
+    sessions = gs.get_maze_sessions(
+        subject_IDs="all",
+        maze_names="all",
+        days_on_maze=days_on_maze,
+        with_data=["navigation_df", "navigation_spike_rates_df", "cluster_metrics", "cluster_distance_tuning_metrics"],
+    )
+    tuning_dfs = []
+    for session in sessions:
+        if verbose:
+            print(session.name)
+        distance_tuning_df = _get_session_distance_tuning(session)
+        distance_tuning_df = distance_tuning_df.reset_index()
+        distance_tuning_df = distance_tuning_df.rename(columns={"index": "cluster_unique_ID"}, level=0)
+        distance_metrics_df = session.cluster_distance_tuning_metrics
+        df = pd.merge(  # conbine tuning curves and (precomputed) tuning curve fit metrics
+            distance_tuning_df,
+            distance_metrics_df.xs("gamma_4p", axis=1, drop_level=False),
+            on="cluster_unique_ID",
+            how="inner",
+        )
+        # filter for distance tuned, split half corr, and r2
+        df = df[(df.distance_tuned) & (df.split_half_corr.value.gt(min_split_half_corr)) & (df[fit].r2 > min_r2)]
+        tuning_dfs.append(df)
+    population_tuning_df = pd.concat(tuning_dfs, axis=0).reset_index(drop=True)
+    return population_tuning_df
+
+
+def _get_session_distance_tuning(
+    session,
+    metrics=("distance_to_goal", "geodesic"),
+    bin_spacing=0.04,
+    n_bins=40,
+    max_steps_to_goal=30,
+    moving_only=False,
+):
+    """ """
+    navigation_rates_df = session.get_navigation_activity_df(type="rates", cluster_kwargs={"single_units": True})
+    # deal with moving only
+    if moving_only:
+        navigation_rates_df = navigation_rates_df[navigation_rates_df.moving]
+    if max_steps_to_goal is not None:
+        navigation_rates_df = navigation_rates_df[navigation_rates_df.steps_to_goal.future < max_steps_to_goal]
+    # remove frames where distance is above max (treat as outliers)
+    if metrics[0] == "distance_to_goal":
+        max_distance = dd.get_distance_percentile(metrics, 0.85)
+        n_bins = int(max_distance / bin_spacing)
+        navigation_rates_df = navigation_rates_df[navigation_rates_df[metrics] < max_distance]
+    # bin distances
+    navigation_rates_df.loc[:, ("distance_bin", "")] = pd.cut(
+        navigation_rates_df[metrics], bins=n_bins, include_lowest=True
+    ).to_numpy()
+    # average over frames in each bin over trials
+    trial_av_rates = navigation_rates_df.groupby(["trial", "distance_bin"], observed=True).firing_rate.mean()
+    distance_tuning_df = (
+        trial_av_rates.groupby(["distance_bin"]).mean().firing_rate.T
+    )  # cluster x distance_bins (average over trials)
+    distance_tuning_df.columns = pd.MultiIndex.from_product(
+        [["distance_to_goal"], [b.mid for b in distance_tuning_df.columns]]
+    )
+    return distance_tuning_df
 
 
 # %% Curve fit summary and plotting functions
