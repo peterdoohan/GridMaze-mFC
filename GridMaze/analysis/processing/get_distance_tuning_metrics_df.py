a080bdbce438e5dd7fd1ecdaab9e892bf52476c9
Peter Doohan <peterdoohan0@gmail.com>
2025-05-26 10:57:59 +0000

    updated with distance metrics fits with cv gamma 4p option. Note runs alot slower now.

--- GridMaze/analysis/processing/get_distance_tuning_metrics_df.py
+++ GridMaze/analysis/processing/get_distance_tuning_metrics_df.py
@@ -35,8 +35,8 @@ def get_distance_tuning_metrics_df(
     distance_metrics=("distance_to_goal", "geodesic"),
     max_steps_to_goal=30,
     moving_only=False,
-    bin_spacing=0.05,
-    alpha=0.01,
+    bin_spacing=0.1,
+    alpha=0.05,
 ):
     """ """
     # load data
@@ -55,17 +55,20 @@ def get_distance_tuning_metrics_df(
         [("goal", ""), ("trial", ""), ("moving", ""), ("steps_to_goal", "future"), distance_metrics]
     ].droplevel(1, axis=1)
     curve_fit_fns = [gamma_2p, gamma_4p, gaussian_2p, gaussian_4p, polynomial_4p]
-    cols = [
-        ("single_unit", ""),
-        ("distance_tuned", ""),
-        ("split_half_corr", "value"),
-        ("split_half_corr", "pvalue"),
-    ] + [(fn.__name__, x) for fn in curve_fit_fns for x in _get_param_names(fn)]
+    curve_fit_cv_fns = [gamma_4p]
+    cols = (
+        [("single_unit", ""), ("distance_tuned", ""), ("split_half_corr", "value"), ("split_half_corr", "pvalue")]
+        + [(fn.__name__, x) for fn in curve_fit_fns for x in _get_param_names(fn)]
+        + [(fn.__name__ + "_cv", x) for fn in curve_fit_cv_fns for x in _get_param_names(fn) + ["p_value", "sig"]]
+    )
     metrics_df = pd.DataFrame(index=cluster_unique_IDs, columns=pd.MultiIndex.from_tuples(cols), data=np.nan)
     # fix boolian dtype cols
     for col in [("single_unit", ""), ("distance_tuned", "")]:
         metrics_df[col] = metrics_df[col].astype(bool)
         metrics_df[col] = False  # false by default
+    for fn in curve_fit_cv_fns:
+        metrics_df.loc[:, (fn.__name__ + "_cv", "sig")].astype(bool)
+    # loop through single unit, calculate distance tuning metrics
     for cluster in single_units:
         metrics_df.loc[cluster, ("single_unit", "")] = True
         cluster_rates = navigation_spike_rates_df.xs(cluster, level=1, axis=1)
@@ -83,13 +86,18 @@ def get_distance_tuning_metrics_df(
         metrics_df.loc[cluster, ("distance_tuned", "")] = sig
         if sig:
             print(cluster)
-            tc = distance_tuning_df.distance
-            for fit_fn, sign in [(gamma_4p, "neg")]:  # curve_fit_fns:
-                params = tuning_curve_fit_cv(tc, fit_fn, sign, plot=True)
+            tuning_df = distance_tuning_df.distance
+            for fit_fn in curve_fit_cv_fns:  # curve_fit_fns:
+                params = tuning_curve_fit_cv(tuning_df, fit_fn, plot=True)
+                for param, value in params.items():
+                    fn_name = fit_fn.__name__
+                    metrics_df.loc[cluster, (fn_name + "_cv", param)] = value
+            tc = tuning_df.mean()
+            for fit_fn in curve_fit_fns:
+                params = tuning_curve_fit(tc, fit_fn, plot=True)
                 for param, value in params.items():
                     fn_name = fit_fn.__name__
                     metrics_df.loc[cluster, (fn_name, param)] = value
-    return
     # return
     metrics_df.reset_index(inplace=True)
     metrics_df.rename(columns={"index": "cluster_unique_ID"}, inplace=True)
@@ -118,9 +126,18 @@ def _get_distance_tuning_metrics(distance_tuning_df, n_reps=50, alpha=0.01):
 # %% Curve fitting functions
 
 
-def tuning_curve_fit_cv(tuning_df, fn, sign, n_splits=20, n_inits=3, alpha=0.01, plot=False, verbose=False):
-    """ """
-    init_range, bounds, param_names = _get_init_range(fn, sign), _get_bounds(fn, sign), _get_param_names(fn)
+def tuning_curve_fit_cv(tuning_df, fn, n_splits=30, n_inits=5, alpha=0.05, plot=False, verbose=False):
+    """
+    Calculates distance tuning curves from split halfs of the data (split over trials).
+    Find fn fit params from the first half of the data and calculate r2 on the second half, over
+    n_splits.
+
+    Note if size param can be both negative and positive across split halve fits.
+    If the number of positive and negative fits are equal, the one with the higher r2 is taken (+/- 1).
+    If the number of positive and negative fits are not equal, the most common is taken.
+    """
+    assert fn.__name__ in ["gamma_4p", "gaussian_4p"], "Only gamma_4p and gaussian_4p are supported for CV fitting."
+    init_range, bounds, param_names = _get_init_range(fn), _get_bounds(fn), _get_param_names(fn)
     x = tuning_df.columns.values.astype(float)
     idx = tuning_df.index.values
     mid = len(idx) // 2
@@ -155,8 +172,25 @@ def tuning_curve_fit_cv(tuning_df, fn, sign, n_splits=20, n_inits=3, alpha=0.01,
         if verbose:
             print(f"best fit: {best_fit}")
         split_fits.append(best_fit)
-    mean_params = pd.DataFrame(split_fits).median().to_dict()
-    result = ttest_1samp([fit["r2"] for fit in split_fits], 0, alternative="greater")
+    splits_df = pd.DataFrame(split_fits)
+    # logic: if number of pos and neg fits are equal +/- 1 take the one with the higher r2 (usually bad fit)
+    pos_fits = splits_df[splits_df["size"] > 0]
+    neg_fits = splits_df[splits_df["size"] < 0]
+    n_pos, n_neg = len(pos_fits), len(neg_fits)
+    if n_pos in list(range(n_neg - 1, n_neg + 2)):
+        pos_r2 = pos_fits["r2"].mean()
+        neg_r2 = neg_fits["r2"].mean()
+        if pos_r2 > neg_r2:
+            mean_params = pos_fits.median().to_dict()
+        else:
+            mean_params = neg_fits.median().to_dict()
+    # logic: if number of pos and neg fits are not equal (eg outlier), take the most common
+    else:
+        if n_pos > n_neg:
+            mean_params = pos_fits.median().to_dict()
+        else:
+            mean_params = neg_fits.median().to_dict()
+    result = ttest_1samp(splits_df["r2"].values, 0, alternative="greater")  # test from all fits (if size pos or neg)
     mean_params["p_value"] = result.pvalue
     mean_params["sig"] = True if mean_params["p_value"] < alpha else False
     if plot:
@@ -223,16 +257,13 @@ def tuning_curve_fit(
     return best_fit
 
 
-def _get_init_range(fn, sign="pos"):
+def _get_init_range(fn):
     """ """
     fn_name = fn.__name__
     if fn_name == "gamma_2p":
-        p0 = [[-1, 1], [0, 1]]  # size, shape
+        p0 = [[-5, 5], [0, 1]]  # size, shape
     elif fn_name == "gamma_4p":
-        if sign == "pos":
-            p0 = [[0, 1], [0.1, 10], [0.1, 1], [0, 3]]  # size, shape, scale, shift
-        else:  # neg
-            p0 = [[-1, 0], [0.1, 10], [0.1, 1], [0, 3]]  # size, shape, scale, shift
+        p0 = [[-5, 5], [0.1, 10], [0.1, 1], [0, 3]]  # size, shape, scale, shift
     elif fn_name == "gaussian_2p":
         p0 = [[0, 5], [0, 2]]  # amplitude, mean
     elif fn_name == "gaussian_4p":
@@ -244,16 +275,13 @@ def _get_init_range(fn, sign="pos"):
     return p0
 
 
-def _get_bounds(fn, sign="pos"):
+def _get_bounds(fn):
     """ """
     fn_name = fn.__name__
     if fn_name == "gamma_2p":
         bounds = [[-np.inf, 0], [np.inf, np.inf]]  # size, shape
     elif fn_name == "gamma_4p":
-        if sign == "pos":
-            bounds = [[0, 0.05, 0.05, -100], [np.inf, 20, 20, 100]]  # size, shape, scale, shift
-        else:  # neg
-            bounds = [[-np.inf, 0.05, 0.05, -100], [0, 20, 20, 100]]
+        bounds = [[-np.inf, 0.05, 0.05, -100], [np.inf, 20, 2, 100]]  # size, shape, scale, shift
     elif fn_name == "gaussian_2p":
         bounds = [(-np.inf, -np.inf), (np.inf, np.inf)]
     elif fn_name == "gaussian_4p":
