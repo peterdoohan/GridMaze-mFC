fe8a292965e84ad3926218ae0fd070c867e3410f
Peter Doohan <peterdoohan0@gmail.com>
2025-06-06 10:21:37 +0000

    removing data generation functions that now live in separate folders

--- GridMaze/analysis/place_direction/efficient_coding.py
+++ GridMaze/analysis/place_direction/efficient_coding.py
@@ -354,112 +354,6 @@ def get_joint_neural_behaviour_place_direction_dfs(sessions, n_splits=5, test_si
     return X
 
 
-# %% Behaviour Sequences
-
-
-def _get_behavioural_sequences(
-    sessions,
-    max_steps_from_goal=DATA_FILTER_KWARGS["max_steps_from_goal"],
-    synthetic=False,
-):
-    """
-    Behavioural sequences are binary vectors of place-direction pairs visited in each trial
-    if synthetic in ["optimal", "random_diffusion", "forward_diffusion"] then the behaviour is generated
-    synthetically (for control analyses)
-    """
-    trials_sequences = []
-    place_direction2idx = {_pd: i for i, _pd in enumerate(mr.get_maze_place_direction_pairs(sessions[0].simple_maze()))}
-    for session in sessions:
-        # load
-        if not synthetic:
-            trajectories_df = session.trajectory_decisions_df
-        else:
-            trajectories_df = cb.get_synthetic_behaviour(session, policy=synthetic)
-        # filter
-        trajectories_df = trajectories_df[(trajectories_df.trial_phase == "navigation")]
-        trajectories_df = trajectories_df[(trajectories_df.maze_position.notnull())]
-        trajectories_df = trajectories_df[(trajectories_df.action.notnull())]
-        if max_steps_from_goal is not None:
-            trajectories_df = trajectories_df[(trajectories_df.steps_to_goal.lt(max_steps_from_goal))]
-        # loop over trials to construct sequence vectors (binary in each place-direction, 1 if visited)
-        trials = trajectories_df.trial.unique()
-        session_sequences = np.zeros((len(trials), len(place_direction2idx)), dtype=int)
-        for i, trial in enumerate(trials):
-            trial_df = trajectories_df[trajectories_df.trial == trial]
-            place_direction_sequence = list(zip(trial_df.maze_position, trial_df.action))
-            for j in place_direction_sequence:
-                session_sequences[i, place_direction2idx[j]] += 1
-        trials_sequences.append(session_sequences)
-    behaviour_df = pd.DataFrame(
-        data=np.vstack(trials_sequences),
-        columns=pd.MultiIndex.from_tuples(place_direction2idx.keys(), names=["maze_position", "direction"]),
-    )
-    return behaviour_df.sort_index(axis=1)
-
-
-def _get_behavioural_sequences_ALT(sessions, filter_kwargs=DATA_FILTER_KWARGS):
-    """
-    Behavioural sequences defined over frames not node-egde-node transitions as in _get_behavioural_sequences
-
-    Slighly slower var exp, although very slighly
-    """
-    trial_sequences = []
-    place_direction2idx = {_pd: i for i, _pd in enumerate(mr.get_maze_place_direction_pairs(sessions[0].simple_maze()))}
-    for session in sessions:
-        navigation_df = session.navigation_df
-        navigation_df = filt.filter_navigation_rates_df(
-            navigation_df,
-            navigation_only=filter_kwargs["navigation_only"],
-            moving_only=filter_kwargs["moving_only"],
-            exclude_time_at_goal=filter_kwargs["exclude_time_at_goal"],
-            max_steps_to_goal=filter_kwargs["max_steps_from_goal"],
-        )
-        # filter edge cases that lack place-direction information
-        navigation_df = navigation_df[navigation_df.maze_position.simple.notnull()]
-        navigation_df = navigation_df[navigation_df.cardinal_movement_direction.notnull()]
-        trials = navigation_df.trial.unique()
-        # build binary reps of trails in place-direction space
-        session_sequences = np.zeros((len(trials), len(place_direction2idx)), dtype=int)
-        for i, trial in enumerate(trials):
-            trial_df = navigation_df[navigation_df.trial == trial]
-            place_direction_sequence = list(zip(trial_df.maze_position.simple, trial_df.cardinal_movement_direction))
-            for j in place_direction_sequence:
-                session_sequences[i, place_direction2idx[j]] += 1
-        trial_sequences.append(session_sequences)
-    behaviour_df = pd.DataFrame(
-        data=np.vstack(trial_sequences),
-        columns=pd.MultiIndex.from_tuples(place_direction2idx.keys(), names=["maze_position", "direction"]),
-    )
-    return behaviour_df.sort_index(axis=1)
-
-
-# %% Neural Tuning Functions
-
-
-def _get_neural_tuning(
-    sessions,
-    filter_kwargs=DATA_FILTER_KWARGS,
-    min_firing_rate=1,  # Hz
-):
-    """
-    min_firing_rate filters for neurons inactive during navigation
-    """
-    all_place_direction_rates = []
-    for session in sessions:
-        # load
-        simple_maze = session.simple_maze()
-        navigation_rates_df = session.get_navigation_activity_df(
-            type="rates", cluster_kwargs={"single_units": True, "multi_units": False}
-        )
-        # filter & calculate average firing rates in each place-direction
-        place_direction_rates = _get_place_direction_df(
-            simple_maze, navigation_rates_df, **filter_kwargs  # [n_neurons, n_place_directions]
-        )
-        # exclude neurons with low firing rate during navigation
-        all_place_direction_rates.append(place_direction_rates[place_direction_rates.mean(axis=1).gt(min_firing_rate)])
-    return pd.concat(all_place_direction_rates, axis=0).sort_index(axis=1)
-
-
 # %% load data
 
 
@@ -488,6 +382,7 @@ def get_analysis_sessions(subject, maze, late=True):
             "navigation_spike_rates_df",
             "trajectory_decisions_df",
             "cluster_metrics",
+            "cluster_place_direction_tuning_metrics",
         ],
     )
     return sessions
