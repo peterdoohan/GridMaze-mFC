f75dc5ecaff7a8438cb2e904215f279d41128db0
Peter Doohan <peterdoohan0@gmail.com>
2025-06-17 10:10:58 +0000

    c

--- GridMaze/analysis/processing/get_action_tuning_metrics_df.py
+++ GridMaze/analysis/processing/get_action_tuning_metrics_df.py
@@ -5,6 +5,9 @@ Library for visualising population tuning aligned to egocentric actions
 # %% Imports
 import numpy as np
 import pandas as pd
+from regex import B
+from scipy.stats import ttest_1samp, zscore
+
 
 from GridMaze.analysis.cluster_tuning import actions as act
 from GridMaze.analysis.core import load_data
@@ -49,22 +52,32 @@ def test(processed_data_path, analysis_data_path, forced_only=True, window=(-3, 
         action_tuning_df = pd.concat(
             [action_tuning_df.drop(columns=["action_aligned_rates"], level=0), ds_rates], axis=1
         )
-    if forced_only:
-        action_tuning_df = action_tuning_df[action_tuning_df.choice_degree.gt(2)]
+    # if forced_only:
+    #     action_tuning_df = action_tuning_df[action_tuning_df.choice_degree.gt(2)]
     cluster_unique_IDs = action_tuning_df.cluster_unique_ID.unique()
+    # metrics df
     for cluster in cluster_unique_IDs:
+        if cluster not in single_units:
+            continue
         cluster_df = action_tuning_df[action_tuning_df.cluster_unique_ID == cluster]
+        return cluster_df
         split_corrs = get_split_half_corr(cluster_df)
         mean_split_corr = np.mean(split_corrs)
 
 
-def get_split_half_corr(cluster_df, n=100):
+def get_action_split_half_corr(cluster_df, action_type="free", n=100, alpha=0.01):
     """ """
+    if action_type == "free":
+        df = cluster_df[cluster_df.choice_degree.gt(2)]
+    elif action_type == "forced":
+        df = cluster_df[cluster_df.choice_degree.le(2)]
+    else:
+        raise ValueError("action_type must be 'free' or 'forced'")
     actions = ["turn_left", "turn_right", "go_forward"]
     left_df, right_df, forward_df = [cluster_df[cluster_df.basic_action == action] for action in actions]
     left_ids, right_ids, forward_ids = [df.action_number.values for df in [left_df, right_df, forward_df]]
     mid_left, mid_right, mid_forward = [len(ids) // 2 for ids in [left_ids, right_ids, forward_ids]]
-    results = []
+    corrs = []
     for i in range(n):
         split_1_tuning, split_2_tuning = [], []
         for df, a_ids, mid in zip(
@@ -87,6 +100,51 @@ def get_split_half_corr(cluster_df, n=100):
         split_1 = np.hstack([LF_1, RF_1, LR_1])
         split_2 = np.hstack([LF_2, RF_2, LR_2])
         split_corr = np.corrcoef(split_1, split_2)[0, 1]
-        results.append(split_corr)
+        corrs.append(split_corr)
+    result = ttest_1samp(corrs, 0, alternative="greater")
+    p_val = result.pvalue
+    mean_corr = np.mean(corrs)
+    sig = True if p_val < alpha else False
+    return mean_corr, sig, p_val
 
-    return results
+
+def get_free_forced_split_half_corr(cluster_df, n=100, alpha=0.01):
+    """ """
+    free_df = cluster_df[cluster_df.choice_degree.gt(2)]
+    forced_df = cluster_df[cluster_df.choice_degree.le(2)]
+    actions = ["turn_left", "turn_right", "go_forward"]
+    # data for free choice actions
+    free_dfs = [free_df[free_df.basic_action == action] for action in actions]
+    free_ids = [df.action_number.values for df in free_dfs]
+    free_mids = [len(ids) // 2 for ids in free_ids]
+    # data for forced choice actions
+    forced_dfs = [forced_df[forced_df.basic_action == action] for action in actions]
+    forced_ids = [df.action_number.values for df in forced_dfs]
+    forced_mids = [len(ids) // 2 for ids in forced_ids]
+    corrs = []
+    for i in range(n):
+        split_1, split_2 = [], []  # (free, forced)
+        for dfs, ids, mids in [(free_dfs, free_ids, free_mids), (forced_dfs, forced_ids, forced_mids)]:
+            split_1_tuning, split_2_tuning = [], []
+            for df, a_ids, mid in zip(dfs, ids, mids):
+                shuffle_ids = a_ids.copy()
+                np.random.shuffle(shuffle_ids)
+                split_1_ids = shuffle_ids[:mid]
+                split_2_ids = shuffle_ids[mid:]
+                split_1_tuning.append(df[df.action_number.isin(split_1_ids)].action_aligned_rates.mean())
+                split_2_tuning.append(df[df.action_number.isin(split_2_ids)].action_aligned_rates.mean())
+            split_1.append(split_1_tuning)  # left, right, forward
+            split_2.append(split_2_tuning)
+        free_1, forced_1 = split_1
+        free_2, forced_2 = split_2
+        free_m_forced_1 = np.hstack(
+            [a - b for a, b in zip(free_1, forced_1)]
+        )  # free_L - forced_L, free_R - forced_R, etc.
+        free_m_forced_2 = np.hstack([a - b for a, b in zip(free_2, forced_2)])
+        split_corr = np.corrcoef(free_m_forced_1, free_m_forced_2)[0, 1]
+        corrs.append(split_corr)
+    result = ttest_1samp(corrs, 0, alternative="greater")
+    p_val = result.pvalue
+    mean_corr = np.mean(corrs)
+    sig = True if p_val < alpha else False
+    return mean_corr, sig, p_val
