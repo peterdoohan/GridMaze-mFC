a3bace3f6c67d69defc9902cde45c31c07d55120
Peter Doohan <peterdoohan0@gmail.com>
2025-07-11 14:18:15 +0000

    new analysis to look at the population level if distance tuning is systematically shifted as a function of theta phase

---
+++ GridMaze/analysis/distance_to_goal/theta_mod_tuning.py
@@ -0,0 +1,20 @@
+"""
+Is there a systematic shift in distance tuning curves across theta phases (peak vs trough)?
+@peterdoohan
+"""
+
+# %% Imports
+import numpy as np
+import pandas as pd
+
+from GridMaze.analysis.cluster_tuning import distance_to_goal as dtg
+
+# %% Global Variables
+
+
+# %% Function
+
+
+def test():
+    """ """
+    return
