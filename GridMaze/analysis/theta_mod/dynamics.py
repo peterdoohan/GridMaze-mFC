f17738adbfad4c0b1aac187841d271c3c8a8c8a1
Peter Doohan <peterdoohan0@gmail.com>
2025-08-01 14:33:41 +0000

    c

---
+++ GridMaze/analysis/theta_mod/dynamics.py
@@ -0,0 +1,12 @@
+"""
+Wouldn't it be cool if we could measure theta modulation over the abitraty neural representations
+as behaviour unfolds?
+"""
+
+# %% Imports
+
+
+# %% Global Variables
+
+
+# %% Functions
