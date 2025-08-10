78ade2b672299df8171a6c5c9c3bbaffcf8d894a
Peter Doohan <peterdoohan0@gmail.com>
2025-08-10 21:11:34 +0000

    c

--- GridMaze/analysis/theta_mod/goal_decoding.py
+++ GridMaze/analysis/theta_mod/goal_decoding.py
@@ -19,28 +19,40 @@ from GridMaze.analysis.core import convert
 
 FRAME_RATE = 60
 
+# %%
+
+
+def get_session_theta_phase_goal_decoding():
+    return
+
+
+def get_session_theta_mod_goal_decoding():
+    """
+    Within session compare goal decoding from feature = n_neurons OR
+    features = n_neurons x n_theta_phases. Need xval per fold opt regularisation
+    bc of different number of features
+    """
+    return
+
+
 # %% Functions
 
 
-def get_event_aligned_input_data(
+def get_input_data(
     session,
     event="cue",
     resolution=0.5,
     window=(-10, 10),
     include_multi_units=True,
-    binning_method="uniform",
-    n_bins=25,
-    max_steps_to_goal=25,
 ):
     """
     Returns a dataframe with spike counts aligned to event (cue & reward) times.
     """
     # load data
-    simple_maze = session.simple_maze()
     session_info = session.session_info
     trials_df = session.trials_df
     navigation_df = session.navigation_df
-    spike_counts_df = session.navigation_spike_counts_df.reset_index(drop=True)
+    theta_spike_counts_df = session.navigation_theta_spike_counts_df.reset_index(drop=True)
     # filter clusters
     keep_clusters = gc.filter_clusters(
         session.cluster_metrics,
@@ -49,24 +61,22 @@ def get_event_aligned_input_data(
         single_units=True,
         multi_units=include_multi_units,
     )
-    spike_counts_df = spike_counts_df[spike_counts_df.columns[spike_counts_df.spike_count.columns.isin(keep_clusters)]]
+    theta_spike_counts_df = theta_spike_counts_df[
+        theta_spike_counts_df.columns[theta_spike_counts_df.columns.get_level_values(1).isin(keep_clusters)]
+    ]
     # get rates aligned to event
     frames_before, frames_after = int(window[0] * FRAME_RATE), int(window[1] * FRAME_RATE)
     event_times = trials_df.set_index("trial").time[event]
     trial2goal = trials_df.set_index("trial").goal
-    # precalculate distances to goal
-    extended_simple_maze = mr.get_extended_simple_maze(simple_maze)
-    path_distances = dict(nx.all_pairs_dijkstra_path_length(extended_simple_maze, weight="weight"))
-    label2coord = mr.get_maze_label2coord(simple_maze)
     nav_info_dfs, spike_count_dfs = [], []
     for trial, event_time in event_times.items():
         event_frame = (navigation_df.time - event_time).abs().argmin()
         nav_aligned_df = navigation_df.iloc[event_frame + frames_before : event_frame + frames_after].reset_index(
             drop=True
         )
-        spikes_aligned_df = spike_counts_df.iloc[event_frame + frames_before : event_frame + frames_after].reset_index(
-            drop=True
-        )
+        spikes_aligned_df = theta_spike_counts_df.iloc[
+            event_frame + frames_before : event_frame + frames_after
+        ].reset_index(drop=True)
         # downsample to speficied resolution
         ds_nav_aligned_df, ds_spikes_aligned_df = ds.downsample_nav_spikes_data(
             nav_aligned_df, spikes_aligned_df, resolution
@@ -80,12 +90,6 @@ def get_event_aligned_input_data(
         # update distnace outside navigation where they are not defined (use shortest path
         # upcoming goal (event=cue) or shortest path to just visted goal (event=reward))
         ds_nav_aligned_df[("goal", "")] = trial2goal[trial]
-        outside_trial_mask = (ds_nav_aligned_df.trial != trial) | (ds_nav_aligned_df.trial_phase != "navigation")
-        pos_coords = ds_nav_aligned_df.loc[outside_trial_mask, ("maze_position", "simple")].map(label2coord)
-        goal_coords = ds_nav_aligned_df.loc[outside_trial_mask, ("goal", "")].map(label2coord)
-        ds_nav_aligned_df.loc[outside_trial_mask, ("steps_to_goal", "future")] = [
-            path_distances[src][dst] for src, dst in zip(pos_coords, goal_coords)
-        ]
         # update trial info so it is consistent across all aligned times
         ds_nav_aligned_df[("trial", "")] = trial
         ds_nav_aligned_df[("trial_unique_ID", "")] = convert.trial2trial_unique_ID(session_info, trial)
@@ -94,15 +98,7 @@ def get_event_aligned_input_data(
     # combine over trials
     nav_info_df = pd.concat(nav_info_dfs, axis=0).reset_index(drop=True)
     spike_count_df = pd.concat(spike_count_dfs, axis=0).reset_index(drop=True)
-    # add distance bins info
-    bins = convert._get_distance_bins(
-        binning_method=binning_method,
-        n_distance_bins=n_bins,
-        distance_metrics=("steps_to_goal", "future"),
-        max_distance=max_steps_to_goal,
-    )
-    nav_info_df[("steps_to_goal", "bin")] = pd.cut(nav_info_df.steps_to_goal.future, bins=bins)
-    nav_info_df[("steps_to_goal", "bin_mid")] = nav_info_df.steps_to_goal.bin.apply(lambda x: x.mid).astype(float)
     # combine nav_info and spike counts
+    nav_info_df.columns = pd.MultiIndex.from_tuples([(*c, "") for c in nav_info_df.columns])
     event_aligned_nav_rates_df = pd.concat([nav_info_df, spike_count_df], axis=1)
     return event_aligned_nav_rates_df
