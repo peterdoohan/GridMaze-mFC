a2bc69c0c9e98115106b693514b58225899f970e
Peter Doohan <peterdoohan0@gmail.com>
2025-08-14 13:27:45 +0000

    new lib to look at theta amp across channels and depths for each subject

---
+++ GridMaze/analysis/lfp/theta_amp.py
@@ -0,0 +1,17 @@
+"""
+Test theta amplitude at different depths and shanks (A->P) across subjects
+"""
+
+# %% Imports
+
+
+# %% Globs
+
+
+# %% Functions
+
+
+def test(session):
+    """ """
+
+    return
