f1c70be57903c3fbb851a403bb213dce39fe8a68
Peter Doohan <peterdoohan0@gmail.com>
2025-08-20 21:24:22 +0000

    finalised plotting for simple version of goal decoding analysis

--- GridMaze/analysis/goal_coding/simple_decoding.py
+++ GridMaze/analysis/goal_coding/simple_decoding.py
@@ -1,30 +1,195 @@
-""" """
+"""
+Can we decode the goal from mFC? Let's start simple, LogReg decoding on session level data
+@peterdoohan
+"""
 
 # %% Imports
-
 import json
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
-import seaborn as sns
 from scipy.stats import ttest_1samp
 from statsmodels.stats.multitest import multipletests
 from sklearn.preprocessing import StandardScaler
-from GridMaze.analysis.core import permute
+from scipy.ndimage import gaussian_filter1d
 
 from GridMaze.analysis.core import get_sessions as gs
 from GridMaze.analysis.core import get_clusters as gc
 from GridMaze.analysis.goal_coding import decoding_utils as du
 
 # %% Global Variables
-from GridMaze.paths import EXPERIMENT_INFO_PATH, RESULTS_PATH
+from GridMaze.paths import ANALYSIS_INFO_PATH, RESULTS_PATH
+
+with open(ANALYSIS_INFO_PATH / "intra_trial_interval_times.json", "r") as f:
+    INTRA_TRIAL_INTERVAL_TIMES = json.load(f)
 
 RESULTS_DIR = RESULTS_PATH / "simple_decoding"
 
-TRIAL_ALIGNED_RATES_FS = 0.04
+MAZE_NAMES = ["maze_1", "maze_2", "rooms_maze"]
+GOAL_SUBSETS = ["subset_1", "subset_2", "all"]
 
 # %%  plotting
+
+
+def plot_event_aligned_decoding_summary(summary_df, axes=None):
+    """ """
+    if axes is None:
+        f, axes = plt.subplots(3, 6, figsize=(6, 3), sharey=True, sharex=True)
+    for i, maze in enumerate(MAZE_NAMES):
+        for j, goal_subset in enumerate(GOAL_SUBSETS):
+            chance = 1 / 24 if goal_subset == "all" else 1 / 12
+            label = f"{maze} - {goal_subset}"
+            these_axes = axes[i, 2 * j : 2 * j + 2]
+            plot_event_aligned_decoding(
+                summary_df,
+                maze_names=[maze],
+                goal_subsets=[goal_subset],
+                chance=chance,
+                axes=these_axes,
+            )
+            these_axes[0].set_title(label, fontsize=8)
+    return
+
+
+def plot_trial_aligned_decoding_summary(summary_df, axes=None):
+    if axes is None:
+        f, axes = plt.subplots(3, 3, figsize=(6, 3), sharex=True, sharey=True)
+    for i, maze in enumerate(MAZE_NAMES):
+        for j, goal_subset in enumerate(GOAL_SUBSETS):
+            chance = 1 / 24 if goal_subset == "all" else 1 / 12
+            label = f"{maze} - {goal_subset}"
+            ax = axes[i, j]
+            plot_trial_aligned_decoding(
+                summary_df,
+                maze_names=[maze],
+                goal_subsets=[goal_subset],
+                color="deepskyblue",
+                chance=chance,
+                ax=ax,
+            )
+            ax.set_title(label, fontsize=8)
+    return
+
+
+def plot_event_aligned_decoding(
+    summary_df,
+    maze_names=["maze_1", "maze_2"],
+    goal_subsets=["subset_1", "subset_2"],
+    chance=1 / 12,
+    plot_smooth_SD=False,
+    color="deepskyblue",
+    y_max=0.65,
+    axes=None,
+):
+    """ """
+    # set up figure
+    if axes is None:
+        f, axes = plt.subplots(1, 2, figsize=(3, 1.5), sharey=True)
+    for event, ax in zip(["cue", "reward"], axes):
+        ax.axvline(x=0, color="k", ls="--", alpha=0.5)
+        ax.axhline(y=chance, color="k", ls=":", alpha=0.5)
+        ax.set_xlabel(event)
+    axes[0].spines[["top", "right"]].set_visible(False)
+    axes[1].spines[["top", "left", "right"]].set_visible(False)
+    axes[0].set_ylabel("acc.")
+
+    # process data
+    df = summary_df[(summary_df.maze_name.isin(maze_names)) & (summary_df.goal_subset.isin(goal_subsets))]
+    subject_mean_dfs = []
+    for event, ax in zip(["cue", "reward"], axes):
+        event_df = df[df.event == event]
+        if event == "cue":
+            # we don't want to non nav times after the cue (eg, consuming reward)
+            event_df = event_df[~(event_df.timepoint.gt(0) & (event_df.trial_phase != "navigation"))]
+        subject_means = event_df.groupby(["timepoint", "subject_ID"]).accuracy.mean().unstack(level=0)
+        _subject_means_df = subject_means.copy()
+        _subject_means_df.columns = pd.MultiIndex.from_product([[event], _subject_means_df.columns])
+        subject_mean_dfs.append(_subject_means_df)
+        # do stats before smoothing
+        if plot_smooth_SD:
+            subject_means = pd.DataFrame(
+                gaussian_filter1d(subject_means.values, sigma=plot_smooth_SD, axis=1),
+                index=subject_means.index,
+                columns=subject_means.columns,
+            )
+        grand_mean = subject_means.mean().values
+        grand_sem = subject_means.sem().values
+        timepoints = np.sort(event_df.timepoint.unique())
+        ax.plot(timepoints, grand_mean, color=color)
+        ax.fill_between(timepoints, grand_mean - grand_sem, grand_mean + grand_sem, color=color, alpha=0.2)
+    # do stats
+    combined_subject_means = pd.concat(subject_mean_dfs, axis=1)
+    reject, p_values_corr = _timeseries_ttests(combined_subject_means, chance=chance)
+    n_timepoints = timepoints.shape[0]
+    for ax, _reg in zip(axes, [reject[:n_timepoints], reject[n_timepoints:]]):
+        plot_sig(_reg, timepoints, ax, sig_pos=y_max, sig_color=color)
+    return combined_subject_means
+
+
+def plot_trial_aligned_decoding(
+    summary_df,
+    maze_names=["maze_1", "maze_2", "rooms_maze"],
+    goal_subsets=["subset_1", "subset_2"],
+    color="deepskyblue",
+    chance=1 / 12,
+    plot_smooth_SD=False,
+    y_max=0.32,
+    ax=None,
+):
+    """ """
+    # set up figure
+    if ax is None:
+        f, ax = plt.subplots(1, 1, figsize=(3, 1.5))
+    ax.spines[["top", "right"]].set_visible(False)
+    event_times = list(INTRA_TRIAL_INTERVAL_TIMES.values())[:-1]
+    timepoints = np.sort(summary_df.timepoint.unique())
+    event_inds = [np.argmin(np.abs(np.array(timepoints) - time)) for time in event_times]
+    for ind in event_inds:
+        ax.axvline(ind, color="k", ls="--", alpha=0.5)
+    ax.set_xticks(event_inds)
+    ax.set_xticklabels(["Cue", "Reward", "ITI"], rotation=0)
+    ax.axhline(chance, color="k", ls=":", alpha=0.5)
+    ax.set_ylabel("acc.")
+    # process data
+    df = summary_df[(summary_df.maze_name.isin(maze_names)) & (summary_df.goal_subset.isin(goal_subsets))]
+    subject_means = df.groupby(["timepoint", "subject_ID"]).accuracy.mean().unstack(level=0)
+    # do stats before smoothing
+    reject_null = _timeseries_ttests(subject_means, chance)
+    if plot_smooth_SD:
+        subject_means = pd.DataFrame(
+            gaussian_filter1d(subject_means.values, sigma=plot_smooth_SD, axis=1),
+            index=subject_means.index,
+            columns=subject_means.columns,
+        )
+    grand_mean = subject_means.mean().values
+    grand_sem = subject_means.sem().values
+    x_range = np.arange(len(timepoints))
+    ax.plot(x_range, grand_mean, color=color)
+    ax.fill_between(x_range, grand_mean - grand_sem, grand_mean + grand_sem, color=color, alpha=0.2)
+    plot_sig(reject_null, x_range, ax, sig_pos=y_max, sig_color=color)
+
+
+def _timeseries_ttests(subject_means, chance):
+    p_values = []
+    timepoints = subject_means.columns.values
+    for t in timepoints:
+        stat, p = ttest_1samp(subject_means[t], popmean=chance, alternative="greater")
+        p_values.append(p)
+    reject, p_values_corr, _, _ = multipletests(p_values, method="fdr_bh")
+    return reject, p_values_corr
+
+
+def plot_sig(reject, x_range, ax, sig_pos=1, sig_color="red"):
+    if sum(reject) > 0:
+        ax.scatter(
+            x_range[reject],
+            np.ones(len(x_range[reject])) * sig_pos,
+            marker="s",
+            color=sig_color,
+            s=0.75,
+        )
+
 
 # %% pop level functions
 
@@ -178,6 +343,7 @@ def get_event_aligned_goal_decoding(
                 }
             )
             df["fold"] = fold
+            df["event"] = event
             results_dfs.append(df)
     results_df = pd.concat(results_dfs, axis=0)
     results_df.reset_index(drop=True, inplace=True)
@@ -278,9 +444,3 @@ def get_trial_aligned_goal_decoding(
     results_df["day_on_maze"] = session.day_on_maze
     results_df["goal_subset"] = session.goal_subset
     return results_df
-
-
-def get_place_control_aligned_goal_decoding():
-    # use place or place-direction as X to predict goal as control
-
-    return
