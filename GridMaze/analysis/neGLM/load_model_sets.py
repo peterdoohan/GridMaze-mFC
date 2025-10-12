7d07ae1e65f42057ac0e0ccb5418d11d13e2a6a9
Peter Doohan <peterdoohan0@gmail.com>
2025-10-12 16:32:20 +0000

    added func to load permuted model sets

--- GridMaze/analysis/neGLM/load_model_sets.py
+++ GridMaze/analysis/neGLM/load_model_sets.py
@@ -17,30 +17,66 @@ RESULTS_DIR = RESULTS_PATH / "neGLM"
 # %% Functions
 
 
-def load_model_set_cv_scores(model_set, maze_names=["maze_1"], all_completed=True):
-    """ """
+def load_model_set_cv_scores(model_set, maze_names=["maze_1", "maze_2", "rooms_maze"], all_completed=True):
+    """
+    See results/neGLM/{model_set}/model_set_params.json to see find model params
+    Note these results folders have folder structure
+       - model_set/maze_name/model_name/cv_scores.csv (containing cv fit scores for each cluster across folds)
+    """
     model_set_dir = RESULTS_DIR / model_set
     dfs = []
     for maze_name in maze_names:
         _dir = model_set_dir / maze_name
         if not _dir.exists():
             raise FileNotFoundError(f"Model set directory does not exist: {_dir}")
-        results_dirs = [f for f in _dir.iterdir() if f.is_dir()]
-        for results_dir in results_dirs:
-            # check if results have been processed
-            if (results_dir / "DONE.txt").exists():
-                # load cv scores
-                cv_scores_path = results_dir / "cv_scores.csv"
-                df = pd.read_csv(cv_scores_path)
-                model_name = results_dir.name
-                df["model_name"] = model_name
-                dfs.append(df)
-            else:
-                if all_completed:
-                    raise FileNotFoundError(f"Results directory not completed: {results_dir}")
-                else:
-                    continue
+        _dfs = _get_result_dfs(_dir, all_completed=all_completed)
+        dfs.extend(_dfs)
     return pd.concat(dfs, ignore_index=True)
+
+
+def load_permuted_model_set_cv_scores(
+    model_set, maze_names=["maze_1", "maze_2", "rooms_maze"], ps=[0], all_completed=True
+):
+    """
+    See results/neGLM/{model_set}/permuation/model_set_params.json to see find model params
+    Note these results folders have folder structure
+       - model_set/permutation/maze_name/model_name/cv_scores.csv (containing cv fit scores for each cluster
+         where spikes and behaviour data have been randomly circularly permuted)
+    """
+    dfs = []
+    for p in ps:
+        model_set_dir = RESULTS_DIR / model_set / str(p)
+        if not model_set_dir.exists():
+            raise FileNotFoundError(f"Model set directory does not exist: {model_set_dir}")
+        for maze_name in maze_names:
+            _dir = model_set_dir / maze_name
+            if not _dir.exists():
+                raise FileNotFoundError(f"Model set directory does not exist: {_dir}")
+            _dfs = _get_result_dfs(_dir, all_completed=all_completed, permutation=p)
+            dfs.extend(_dfs)
+    return pd.concat(dfs, ignore_index=True)
+
+
+def _get_result_dfs(_dir, all_completed=True, permutation=None):
+    _dfs = []
+    results_dirs = [f for f in _dir.iterdir() if f.is_dir()]
+    for results_dir in results_dirs:
+        # check if results have been processed
+        if (results_dir / "DONE.txt").exists():
+            # load cv scores
+            cv_scores_path = results_dir / "cv_scores.csv"
+            df = pd.read_csv(cv_scores_path)
+            model_name = results_dir.name
+            df["model_name"] = model_name
+            if permutation is not None:
+                df["permutation"] = permutation
+            _dfs.append(df)
+        else:
+            if all_completed:
+                raise FileNotFoundError(f"Results directory not completed: {results_dir}")
+            else:
+                continue
+    return _dfs
 
 
 def load_model(
